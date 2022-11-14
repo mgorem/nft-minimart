409d0d76309a38857ffdcbe993ed20329e8f45a9
omg <oremgitonga22@gmail.com>
2022-11-14 17:11:44 +0300

    additional functions

--- blockchain/contracts/NFTMarket.sol
+++ blockchain/contracts/NFTMarket.sol
@@ -3,21 +3,50 @@ pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "hardhat/console.sol";
+
+struct NFTListing {
+    uint256 price;
+    address seller;
+}
 
 contract NFTMarket is ERC721URIStorage {
     using Counters for Counters.Counter;
-    Counters.Counter private _tokenIds;
+    Counters.Counter private _tokenIDs;
+    using SafeMath for uint256;
+    mapping(uint256 => NFTListing) private _listings;
 
     constructor() ERC721("Orem's NFTs", "ONFT") {}
 
     // function to create NFT
     function createNFT(string calldata tokenURI) public returns (uint256) {
-        _tokenIds.increment();
-        uint256 currentId = _tokenIds.current();
+        _tokenIDs.increment();
+        uint256 currentId = _tokenIDs.current();
 
         _safeMint(msg.sender, currentId);
         _setTokenURI(currentId, tokenURI);
         return currentId;
+    }
+
+    // function to create NFT
+    function listNFT(uint256 tokenID, uint256 price) public {
+        require(price > 0, "NFTMarket: Price must be greater than 0");
+        // approving contract to transfer this token ownership
+        // asserting that the address initiating this transaction is the owner of the nft
+        approve(address(this), tokenID);
+        // transfer ownership of NFT to NFTMarket
+        transferFrom(msg.sender, address(this), tokenID);
+        _listings[tokenID] = NFTListing(price, msg.sender);
+    }
+
+    function buyNFT(uint256 tokenID) public payable {
+        NFTListing memory listing = _listings[tokenID];
+        require(listing.price > 0, "NFTMarket: nft not listed for sale");
+        require(msg.value == listing.price, "NFTMarket: incorrect price");
+        ERC721(address(this)).transferFrom(address(this), msg.sender, tokenID);
+        // clearListing(tokenID);
+        payable(listing.seller).transfer(listing.price.mul(95).div(100));
+        // emit NFTTransfer(tokenID, address(this), msg.sender, "", 0);
     }
 }
--- blockchain/test/NFTMarket.ts
+++ blockchain/test/NFTMarket.ts
@@ -14,9 +14,16 @@ describe("NFTMarket", function () {
     const tokenURI = "https://some-token.uri/";
     const transaction = await nftMarket.createNFT(tokenURI);
     const receipt = await transaction.wait();
-    console.log(receipt);
 
-    // assert nft has been created and its tokenURI
-    // const tokenId = receipt.events[0].args.tokenID;
+    // assert that newly created NFT's token uri is same as the one sent to the createNFT function
+    const tokenID = receipt.events[0].args.tokenId;
+    const mintedTokenURI = await nftMarket.tokenURI(tokenID);
+    expect(mintedTokenURI).to.be.equal(tokenURI);
+
+    // assert that owner of newly created nft is the actual user who initiated the transaction
+    const ownerAddress = await nftMarket.ownerOf(tokenID);
+    const signers = await ethers.getSigners();
+    const currentAddress = await signers[0].getAddress();
+    expect(ownerAddress).to.be.equal(currentAddress);
   })
 });
